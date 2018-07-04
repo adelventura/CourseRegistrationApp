0d067d7426feedf6592de7a20627f5748fbadd52
Ashley DelVentura <ashley.delventura@gmail.com>
2018-07-04 17:14:22 -0400

    button listeners

--- src/com/groupone/ScheduleView.java
+++ src/com/groupone/ScheduleView.java
@@ -2,10 +2,13 @@ package com.groupone;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ScheduleView extends JPanel {
 
     JLabel titleLabel;
+    JButton addButton;
     ClassListView classListView;
     JButton dropButton;
 
@@ -13,12 +16,37 @@ public class ScheduleView extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         titleLabel = new JLabel("Schedule Page", JLabel.LEFT);
+        addButton = new JButton("Add");
         dropButton = new JButton("Drop");
         classListView = new ClassListView();
 
+        ButtonListener listener = new ButtonListener();
+        addButton.addActionListener(listener);
+        dropButton.addActionListener(listener);
+
         add(titleLabel);
+        add(addButton);
         add(classListView);
         add(dropButton);
+    }
+
+    private void add() {
+        JOptionPane.showMessageDialog(null, "todo: go to the add courses page");
+    }
+
+    private void drop() {
+        JOptionPane.showMessageDialog(null, "todo: drop classes");
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Object source = event.getSource();
+            if (source == addButton) {
+                add();
+            } else if (source == dropButton) {
+                drop();
+            }
+        }
     }
 
 }
