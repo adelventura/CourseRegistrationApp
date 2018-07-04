95720cc661f7d29531084828d2ddc1c7eb51d64f
Ashley DelVentura <ashley.delventura@gmail.com>
2018-07-04 17:16:12 -0400

    make the list take an array

--- src/com/groupone/ClassListView.java
+++ src/com/groupone/ClassListView.java
@@ -1,19 +1,19 @@
 package com.groupone;
 
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class ClassListView extends JPanel {
 
     JLabel titleLabel;
 
-    public ClassListView() {
+    public ClassListView(ArrayList<Course> courses) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         titleLabel = new JLabel("Class List", JLabel.LEFT);
 
         add(titleLabel);
-        for (int i = 0; i < 10; i += 1) {
-            String name = "CS" + Integer.toString((int)(Math.random() * 1000));
-            Course course = new Course(name, "Computer Science");
+        for (int i = 0; i < courses.size(); i += 1) {
+            Course course = courses.get(i);
             ClassItemCell cell = new ClassItemCell(course);
             add(cell);
         }
--- src/com/groupone/ScheduleView.java
+++ src/com/groupone/ScheduleView.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class ScheduleView extends JPanel {
 
@@ -18,7 +19,15 @@ public class ScheduleView extends JPanel {
         titleLabel = new JLabel("Schedule Page", JLabel.LEFT);
         addButton = new JButton("Add");
         dropButton = new JButton("Drop");
-        classListView = new ClassListView();
+
+        ArrayList<Course> courses = new ArrayList<>();
+        for (int i = 0; i < 10; i += 1) {
+            String name = "CS" + Integer.toString((int)(Math.random() * 1000));
+            Course course = new Course(name, "Computer Science");
+            courses.add(course);
+        }
+
+        classListView = new ClassListView(courses);
 
         ButtonListener listener = new ButtonListener();
         addButton.addActionListener(listener);
