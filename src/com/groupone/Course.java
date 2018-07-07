60f90046b41d99a615225552e723a7567e381e48
sabrineabd <38770178+sabrineabd@users.noreply.github.com>
2018-07-06 21:01:58 -0400

    Update Course.java

--- src/com/groupone/Course.java
+++ src/com/groupone/Course.java
@@ -1,21 +1,30 @@
 package com.groupone;
+import java.sql.*;
 
 public class Course {
-
+    //I added some things to this
     public String name;
     public String department;
     public int room;
     public String time;
     public String day;
+    public int courseNum;
+    public int credits;
+    public String instructor;
+    
+    public Course(String name, String department, int roomNum, String time, 
+    		String day, int courseNum, int credits, String instructor) {
+		this.name=name;
+		this.department=department;
+		this.room=roomNum;
+		this.time=time;
+		this.courseNum = courseNum;
+		this.day=day;
+		this.credits=credits;
 
-    public Course(String name, String department) {
-        this.name = name;
-        this.department = department;
+}
 
-        // get all this from the database eventually
-        this.room = (int)(Math.random() * 1000);
-        this.time = "1:00 pm - 2:00 pm";
-        this.day = "MWF";
-    }
+
+  
 
 }
