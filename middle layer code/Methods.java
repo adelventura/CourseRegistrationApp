40b4844d97d93b8708eb34ca9bc12492f45d076c
sabrineabd <38770178+sabrineabd@users.noreply.github.com>
2018-07-07 10:34:10 -0400

    Update Methods.java

--- middle layer code/Methods.java
+++ middle layer code/Methods.java
@@ -1,8 +1,12 @@
-//THESE ARE METHODS THAT ARE CALLED IN THE STUDENT CLASS
-
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Methods {
+	
+	/*
+	 * this method is used for connecting to either the table of students 
+	 * and each student's individual course list table in the database
+	 */
 	
 	public static Connection connectToStudentsTable(String user, String pass) {
 		//connection string
@@ -16,6 +20,10 @@ public class Methods {
 		return conn;
 	}
 	
+	/*
+	 * this method connects to the database department table
+	 */
+	
 	public static Connection connectToDeptTable(String user, String pass) {
 		//connection string
 		String url = "jdbc:mysql://localhost:3306/department_tables";
@@ -28,26 +36,24 @@ public class Methods {
 		return conn;
 	}
 	
-	//returns all courses in a specific department as an array list of course objects
-	public static ArrayList<Course> selectAllCoursesFromASpecificDept(String department) {
+	
+	/*
+	 * this method returns an array list of course objects, these course objects
+	 * hold the coures information of all courses of a specified department
+	 */
+	public ArrayList<Course> selectAllCoursesFromASpecificDept(String department) {
 		ArrayList<Course> allDeptCourses = new ArrayList<Course>();
-		
 		if(deptExists(department)) {
 			String sqlCode = "SELECT * FROM `department_tables`.`all_courses` WHERE `department` = '"+department+"'";
-			
 			try(Connection conn = connectToDeptTable("root", "password")){
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sqlCode);
-				ResultSetMetaData rsmd = rs.getMetaData(); 
-				int colCount = rsmd.getColumnCount();  
-				
 				while(rs.next()) {
 					Course course = new Course(rs.getString(2), rs.getString(8), rs.getInt(7), rs.getString(4),
 							rs.getString(3), rs.getInt(1), rs.getInt(5), rs.getString(6));
 					allDeptCourses.add(course);
 				}
 
-				
 			}catch (SQLException e) {
 				System.out.println(e.getMessage());
 			}
@@ -60,11 +66,12 @@ public class Methods {
 		
 	}
 	
-	
-	public static boolean deptExists(String department) {
+	/*
+	 * this method verifies that the department passed through exists
+	 */
+	public boolean deptExists(String department) {
 		//checking to see if dept exists
 		String[] existingDepts = new String[]{"Chemistry","Physics","Math", "Computer Science"};
-		
 		for(int i =0; i<existingDepts.length; i++) {
 			if(existingDepts[i].equals(department)) {
 				return true;
@@ -73,3 +80,8 @@ public class Methods {
 		return false;
 	}
 	
+	
+	
+	
+
+}
